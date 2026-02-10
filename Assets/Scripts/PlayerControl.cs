edb351f510d6d607200d912c8a569cdaa43662f8
jaiacedera <jairahacedera@gmail.com>
2026-02-10 13:02:34 +0800

    new Player Control

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -23,8 +23,8 @@ public class PlayerControl : MonoBehaviour
 
     void Move(Vector2 direction)
     {
-        Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 1));
-        Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2  (1, 0));
+        Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2  (1, 1));
 
         max.x = max.x - 0.225f;
         min.x = min.x + 0.225f;
