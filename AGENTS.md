ec9d0511bb15becfd70e287f9995b4360d95ca1e
jaiacedera <jairahacedera@gmail.com>
2026-02-10 12:58:34 +0800

    scripts and packages

---
+++ AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: trial_spaceshooter
+- Unity version: Unity 6000.3.7f1
+- Active game object:
+  - Name: PlayerGO
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        Move(direction);
+    }
+
+    void Move(Vector2 direction)
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 1));
+        Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2  (1, 0));
+
+        max.x = max.x - 0.225f;
+        min.x = min.x + 0.225f;
+
+        max.y = max.y - 0.285f;
+        min.y = min.y + 0.285f;
+
+        Vector2 pos = transform.position;
+
+        pos = pos + direction * speed * Time.deltaTime;
+
+        pos.x = Mathf.Clamp (pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp (pos.y, min.y, max.y);
+
+        transform.position = pos;
+    }
+}
